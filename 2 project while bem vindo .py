c9e56ff91f640b0c0a4fa92dc2cb9067e41d3f57
Cardoso Matine <cardoso.matine27@gmail.com>
2024-09-10 19:13:11 -0600

    Primeiro commit

---
+++ 2 project while bem vindo .py
@@ -0,0 +1,10 @@
+#projet laço de repetiçao:  boa vindas pra muitos usuarios
+
+nome = None #nao tem nada nessa var
+
+while True: #infinito
+	print ('digite seu nome, ou x para parar:')
+	nome= input()
+	if nome == 'x' or nome == "X":
+		break #quebre, pare o programa
+	print(f' bem vindo , {nome}')
